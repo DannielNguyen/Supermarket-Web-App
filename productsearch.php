7f0f8c5f7ba12144b58a946cf4cdef3953d04b6e
Daniel Nguyen <32965196+DannielNguyen@users.noreply.github.com>
2022-05-02 21:41:43 -0400

    Update productsearch.php

--- productsearch.php
+++ productsearch.php
@@ -4,4 +4,43 @@
     if(isset($_POST["keywords"])) $keywords = $_POST["keywords"];
     $content ="$keywords";
     include_once "templates/sitetemplate.php";
+
+$servername = "localhost";
+$username = "pnguyen91";
+$password = "pnguyen91";
+$dbname = "pnguyen91";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error){
+echo "Could not connect to server\n";
+
+}
+else {
+    echo "Connection established\n";
+}
+    echo "<br>";
+    echo "showing results with keyword ". $keywords;
+    $sql = "SELECT * from inventory WHERE itemname like '%$keywords%'";
+$result = $conn->query($sql);
+
+echo "<h3>Search result</h3>";
+
+if ($result->num_rows > 0) {
+     // output data of each row
+	echo "<table><tr><th>Item Name</th><th>Description</th><th>Price</th><th>Number in stock</th></tr>";
+	while ($row = $result->fetch_assoc()) {
+		echo "<tr><td>".($row['itemname'])."</td>";
+		echo "<td>".($row['description'])."</td>";
+		echo "<td>".($row['price'])."</td>";
+		echo "<td>".($row['numberinstock'])."</td>";
+		echo"</tr>";
+
+	}
+	echo "</table>";
+}
+else{
+    echo "No result with given keywords";
+}
+
+
 ?>
